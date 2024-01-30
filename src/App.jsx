6b7290a81c83c3e96918fc539feb7ed03457c55a
jcgoodwin86 <jcgoodwin86@users.noreply.github.com>
2024-01-30 11:59:04 -0500

    Init project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+/*
+  Break Down of all components to build
+
+  TODO Header
+    TODO Nav
+      TODO Links to pages
+  TODO Main
+    TODO Button to pay
+    TODO Button to transfer
+    TODO Accounts List
+      TODO Main Account Card
+      TODO Expenses Card
+      TODO Savings Card
+
+*/
+
+export default function App() {
+  return (
+    <>
+      <p>Hello World!</p>
+    </>
+  );
+}
