bb2c52af94a0fb71db721643eeabe9bcfc7abd4b
jcgoodwin86 <jcgoodwin86@users.noreply.github.com>
2024-01-31 22:09:27 -0500

    Add global styles and implement initial UI elements to the header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,40 @@
+import styles from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.titleContainer}>
+        <img
+          className={styles.logo}
+          src="img/BestBankLogo.png"
+          alt="best bank logo"
+        />
+        <h1 className={styles.title}>Best Bank</h1>
+      </div>
+      <div className={styles["menu-list"]}>
+        <a
+          className={`${styles.buttonLikeLink} ${styles.activeButton}`}
+          href="/"
+        >
+          Home
+        </a>
+
+        <a className={styles.buttonLikeLink} href="#">
+          Payments
+        </a>
+
+        <a className={styles.buttonLikeLink} href="#">
+          Savings
+        </a>
+
+        <a className={styles.buttonLikeLink} href="#">
+          Financing
+        </a>
+
+        <a className={styles.buttonLikeLink} href="#">
+          Stocks
+        </a>
+      </div>
+    </header>
+  );
+}
